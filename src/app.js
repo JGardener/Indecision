95b99264c6770cbc11fd75771a579b6dad7d2b59
JGardener <jamesgardener92@gmail.com>
2021-05-12 14:14:07 +0100

    Added an app.js in src which will be where we type code. It will compile into public/scripts/app.js which has the vanilla JS

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
-"use strict";
-
 console.log("app.js is running!");
 
 // JSX - Javascript XML
 // A Javascript language extension provided by React - defines templates and injects data
 // into those templates. JSX makes it easy to work with those templates.
 
-var template = React.createElement(
-  "h1",
-  null,
-  "Ahoy hoy"
-);
+var template = <h2>Ahoy hoy</h2>;
 
 var appRoot = document.getElementById('app');
 
