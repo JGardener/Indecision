53ecc0cfa660eb2cd284818d873b051a8a2d65ff
JGardener <jamesgardener92@gmail.com>
2021-05-13 12:17:55 +0100

    Learning conditional rendering using ternary operator to conditionally render various information.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,9 +6,22 @@ console.log("app.js is running!");
 // A Javascript language extension provided by React - defines templates and injects data
 // into those templates. JSX makes it easy to work with those templates.
 
-var textObject = {
+var appInfo = {
   title: "Application Title",
-  subtitle: "Application Subtitle"
+  subtitle: "Application Subtitle",
+  options: ["One", "Two"]
+};
+
+var getSubtitle = function getSubtitle(subtitle) {
+  return subtitle && appInfo.options.length > 0 ? React.createElement(
+    "p",
+    null,
+    "Here are your options"
+  ) : React.createElement(
+    "p",
+    null,
+    "No options"
+  );
 };
 
 var template = React.createElement(
@@ -17,12 +30,12 @@ var template = React.createElement(
   React.createElement(
     "h2",
     null,
-    textObject.title
+    appInfo.title
   ),
   React.createElement(
     "p",
     null,
-    textObject.subtitle
+    getSubtitle(appInfo.subtitle)
   ),
   React.createElement(
     "ol",
@@ -45,30 +58,44 @@ var user = {
   age: 28,
   location: "England"
 };
+var getName = function getName(name) {
+  return name ? React.createElement(
+    "p",
+    null,
+    "Name: ",
+    name
+  ) : React.createElement(
+    "p",
+    null,
+    "Anonymous"
+  );
+};
+var getAge = function getAge(age) {
+  return age && age >= 18 ? React.createElement(
+    "p",
+    null,
+    "Age: ",
+    age
+  ) : undefined;
+};
+var getLocation = function getLocation(location) {
+  return React.createElement(
+    "p",
+    null,
+    "Location: ",
+    location
+  );
+};
 
 var templateTwo = React.createElement(
   "div",
   null,
-  React.createElement(
-    "h1",
-    null,
-    "Name: ",
-    user.name
-  ),
-  React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age
-  ),
-  React.createElement(
-    "p",
-    null,
-    "Location: ",
-    user.location
-  )
+  getName(user.name),
+  getAge(user.age),
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -4,17 +4,21 @@ console.log("app.js is running!");
 // A Javascript language extension provided by React - defines templates and injects data
 // into those templates. JSX makes it easy to work with those templates.
 
-var textObject = {
+var appInfo = {
   title: "Application Title",
-  subtitle: "Application Subtitle"
+  subtitle: "Application Subtitle",
+  options: ["One", "Two"]
 }
+
+const getSubtitle = (subtitle) => subtitle && appInfo.options.length > 0 ? <p>{"Here are your options"}</p> : <p>{"No options"}</p>
+
 
 var template = (
   <div>
     <h2>
-      {textObject.title}
+      {appInfo.title}
     </h2>
-    <p>{textObject.subtitle}</p>
+    <p>{getSubtitle(appInfo.subtitle)}</p>
     <ol>
       <li>Item One</li>
       <li>Item Two</li>
@@ -27,15 +31,19 @@ var user = {
   age: 28,
   location: "England"
 };
+const getName = (name) => name ? <p>Name: {name}</p> : <p>{"Anonymous"}</p>;
+const getAge = (age) => age && age >= 18 ? <p>Age: {age}</p> : undefined;
+const getLocation = (location) => <p>Location: {location}</p>;
 
 var templateTwo = (
   <div>
-    <h1>Name: {user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    {getName(user.name)}
+    {getAge(user.age)}
+    {getLocation(user.location)}
   </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
+// ReactDOM.render(templateTwo, appRoot);
