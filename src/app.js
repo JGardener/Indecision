3d7b702935c75f84295784e35c7113c2fdb9fb79
JGardener <jamesgardener92@gmail.com>
2021-05-26 16:17:43 +0100

    Getting JSX working inside Webpack required some configuration. Also added React and ReactDOM as dependencies. Validator was for playing around and testing

--- src/app.js
+++ src/app.js
@@ -1,26 +1,6 @@
-// import './utils.js';
-// import square from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-import maths, { square, add } from './utils.js'
-import seniorCitizen, { isAdult, canDrink } from './person'
+const template = <p>This is JSX from webpack</p>
 
-console.log("Hello");
-console.log(square(12));
-console.log(add(24,8));
-console.log(isAdult(25));
-console.log(canDrink(13));
-console.log(maths(2,1))
-console.log(seniorCitizen(25))
-
-/* 
-Because subtract is the default export from utils, note the word default, 
-you can name it whatever you want here, but stick with the name of the function
-so it makes sense to other devs and you later. 
-I named it maths
-*/
-
-/* 
-isSenior is coming from the person.js file. 
-Due to it being a default export, I can name it whatever I want here
-I named it seniorCitizen
-*/
+ReactDOM.render(template, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,28 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      // test checks whether a file ends in .js or not.
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
   }
 }
 
+// Gotta teach Webpack how to compile JSX code into normal JS code
+// To do this, you need loaders. 
+//  -> Configuring the way Webpack behaves for specific files. 
+// Step One: New local dependencies!
+//  -> babel-loader@7.1.1 and babel-core@6.25.0
+// Now we set up the loader.
+
+// A recap: this covers from line 13 onward - > module
+// We can't use JSX inside Webpack without teaching Webpack how to run Babel. 
+// Babel will take our JSX and compile it into vanilla JS. 
+// Babel does nothing without configuration, so we had to set it up to do that.
+// The rules section is as follows: 
+// We run a test! If the file ends in .js and doesn't sit inside the node_modules folder, 
+// then run that file through Babel! :)
