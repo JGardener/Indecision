a389baf5d75db576991e0eaa25d3a4ff4b21a689
JGardener <jamesgardener92@gmail.com>
2021-05-28 10:11:38 +0100

    A refactor of the stateless functional components here. They were explicitly returning JSX, when, because of the arrow functions being use, they can be implicitly returned instead.

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Action = (props) => {
-  return (
-    <div>
-      <button 
-      disabled={props.options.length == 0}
-      onClick={props.handleDecision}
-      >
-        What should I do?
-      </button>
-    </div>
-    );
-}
+const Action = (props) => (
+  <div>
+    <button 
+    disabled={props.options.length == 0}
+    onClick={props.handleDecision}
+    >
+      What should I do?
+    </button>
+  </div>  
+)
 
 export default Action;
 
